905852f182688f2c69eadad2df36a3a720ae71b2
Alikley <88429641+Alikley@users.noreply.github.com>
2023-06-03 16:18:37 +0330

    Update Header.jsx

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,21 +1,29 @@
-import {Container,Nav ,Navbar } from 'react-bootstrap/';
-
+import {Container,Nav ,Navbar,NavDropdown } from 'react-bootstrap/';
+import Admin from '../../../hooks/Admin';
+import { useAuth } from '../../../context/Roless';
+import { NavLink } from 'react-router-dom';
 function Header({islogin}) {
+  const { first , setProfile } = useAuth();
+
   return (
     <div>
         <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">کاریار</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">خانه</Nav.Link>
-            <Nav.Link href="/about">درباره ما </Nav.Link>
+           <Nav.Link> <NavLink to="/">خانه</NavLink></Nav.Link>
             {
               islogin?
-              <Nav.Link href="/panel">لیست مهارت اموز</Nav.Link>
+              <NavLink to="/panel">لیست مهارت اموز</NavLink>
               :
-              <Nav.Link href="/login">ورود</Nav.Link>
+              <NavLink to="/login">ورود</NavLink>
 
             }
+            
+            <NavDropdown title="ادمین" id="basic-nav-dropdown">
+              <Admin />
+            
+            </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
